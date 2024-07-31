0b337920615b9b3d267b831bf1a8679ae4f3a902
gjwang <guijiewan@gmail.com>
2024-07-31 22:43:01 +0800

    load s3 key from .env

--- src/bin/s3_service_demo.rs
+++ src/bin/s3_service_demo.rs
@@ -1,17 +1,20 @@
 use std::env;
 use std::path::Path;
 
+use dotenv::dotenv;
 use s3::Bucket;
 use s3::creds::Credentials;
 use s3::Region;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
     // S3 or MinIO configuration
     let endpoint = "http://localhost:9000"; // Use this for MinIO, comment out for S3
     let bucket_name = "my-bucket2";
     let region = "us-east-1";
-    
+
     let aws_access_key_id = env::var("S3_ACCESS_KEY_ID").expect("S3_ACCESS_KEY must be set");
     let aws_secret_access_key = env::var("S3_SECRET_ACCESS_KEY").expect("S3_SECRET_KEY must be set");
 
