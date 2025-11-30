4ff15fb7946f0806a83bded1e97be913818702d5
gjwang <guijiewan@gmail.com>
2025-11-30 15:23:50 +0800

    refactor msg struct

--- src/models.rs
+++ src/models.rs
@@ -6,8 +6,6 @@ pub struct BalanceUpdate {
     pub available: f64,
     pub locked: f64,
     pub total: f64,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub ts_ms: Option<i64>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -21,8 +19,6 @@ pub struct OrderUpdate {
     pub quantity: f64,
     pub filled_quantity: f64,
     pub remaining_quantity: f64,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub ts_ms: Option<i64>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -35,8 +31,6 @@ pub struct PositionUpdate {
     pub liquidation_price: f64,
     pub unrealized_pnl: f64,
     pub leverage: f64,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub ts_ms: Option<i64>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -45,4 +39,12 @@ pub enum UserUpdate {
     Balance(BalanceUpdate),
     Order(OrderUpdate),
     Position(PositionUpdate),
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct StreamMessage {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub ts_ms: Option<i64>,
+    #[serde(flatten)]
+    pub update: UserUpdate,
 }
