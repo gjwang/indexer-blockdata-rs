f61ffee29f78e586ae89f3602af0b8fe0f100233
gjwang <guijiewan@gmail.com>
2025-12-05 01:17:17 +0800

    refactor balance_manager

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,3 +1,11 @@
+pub use api_response::*;
+pub use balance_requests::*;
+pub use client_order::*;
+pub use events::*;
+pub use order_requests::*;
+pub use order_utils::*;
+pub use user_account_manager::*;
+
 pub mod api_response;
 pub mod balance_requests;
 pub mod client_order;
@@ -6,14 +14,8 @@ pub mod order_requests;
 pub mod order_utils;
 pub mod serde_utils;
 pub mod user_account_manager;
+pub mod balance_manager;
 
-pub use api_response::*;
-pub use balance_requests::*;
-pub use client_order::*;
-pub use events::*;
-pub use order_requests::*;
-pub use order_utils::*;
-pub use user_account_manager::*;
 // We don't necessarily need to export serde_utils content globally, but maybe useful.
 // The original code had `mod float_as_string` which was private/local to models.rs but used in structs.
 // Since we used `pub use ...` for others, let's keep it consistent.
